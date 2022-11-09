562dc25d6547ac019258bd9e10fb86b58dbc7ed8
laurenhatchett <laurenhatchett@gmail.com>
2022-11-09 12:58:25 -0500

    create view all finches functionality

--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,24 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+# Add the Finch class & list and view function below the imports
+class Finch:  # Note that parens are optional if not inheriting from another class
+  def __init__(self, name, breed, description, age):
+    self.name = name
+    self.breed = breed
+    self.description = description
+    self.age = age
+
+finches = [
+  Finch('Lolo', 'tabby', 'Kinda rude.', 3),
+  Finch('Sachi', 'tortoiseshell', 'Looks like a turtle.', 0),
+  Finch('Fancy', 'bombay', 'Happy fluff ball.', 4),
+  Finch('Bonk', 'selkirk rex', 'Meows loudly.', 6)
+]
+
+
+
+
 # Create your views here.
 
 def home(request):
@@ -8,3 +26,6 @@ def home(request):
 
 def about(request):
   return render(request, 'about.html')
+
+def finches_index(request):
+  return render(request, 'finches/index.html', { 'finches': finches })
