152fd53c4a9c95f5b9570c8d8b7bf1beb509fc0b
laurenhatchett <laurenhatchett@gmail.com>
2022-11-10 09:21:19 -0500

    add toy model functionality

--- main_app/models.py
+++ main_app/models.py
@@ -40,3 +40,14 @@ class Feeding(models.Model):
   class Meta:
     ordering = ['-date']
 
+
+class Toy(models.Model):
+  name = models.CharField(max_length=50)
+  color = models.CharField(max_length=20)
+
+  def __str__(self):
+    return self.name
+
+  def get_absolute_url(self):
+    return reverse('toys_detail', kwargs={'pk': self.id})
+
