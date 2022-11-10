25a73f703977dcbd17e41a768204a6f406aa1967
laurenhatchett <laurenhatchett@gmail.com>
2022-11-10 10:32:19 -0500

    implement finch & toy association

--- main_app/views.py
+++ main_app/views.py
@@ -18,8 +18,9 @@ def finches_index(request):
 
 def finches_detail(request, finch_id):
   finch = Finch.objects.get(id=finch_id)
+  toys_finch_doesnt_have = Toy.objects.exclude(id__in = finch.toys.all().values_list('id'))
   feeding_form = FeedingForm()
-  return render(request, 'finches/detail.html', { 'finch': finch, 'feeding_form': feeding_form })
+  return render(request, 'finches/detail.html', { 'finch': finch, 'feeding_form': feeding_form, 'toys': toys_finch_doesnt_have })
 
 def add_feeding(request, finch_id):
   form = FeedingForm(request.POST)
@@ -27,6 +28,10 @@ def add_feeding(request, finch_id):
     new_feeding = form.save(commit=False)
     new_feeding.finch_id = finch_id
     new_feeding.save()
+  return redirect('finches_detail', finch_id=finch_id)
+
+def assoc_toy(request, finch_id, toy_id):
+  Finch.objects.get(id=finch_id).toys.add(toy_id)
   return redirect('finches_detail', finch_id=finch_id)
 
 class FinchCreate(CreateView):
