e3696edfb2dded0bb9a6c46784fde90c14b94d15
laurenhatchett <laurenhatchett@gmail.com>
2022-11-10 11:35:49 -0500

    create user to cat relationship & add URLS for authentication

--- main_app/urls.py
+++ main_app/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-  path('', views.home, name='home'),
+  path('', views.Home.as_view(), name='home'),
   path('about/', views.about, name='about'),
   path('finches/', views.finches_index, name='finches_index'),
   path('finches/<int:finch_id>/', views.finches_detail, name='finches_detail'),
@@ -16,4 +16,5 @@ urlpatterns = [
   path('toys/<int:pk>/update/', views.ToyUpdate.as_view(), name='toys_update'),
   path('toys/<int:pk>/delete/', views.ToyDelete.as_view(), name='toys_delete'),
   path('finches/<int:finch_id>/assoc_toy/<int:toy_id>/', views.assoc_toy, name='assoc_toy'),
+  path('accounts/signup/', views.signup, name='signup'),
 ]
